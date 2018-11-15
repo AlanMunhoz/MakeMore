8fe261bc42090a417b931cc9ae9466cb79e0c574
AlanMunhoz <36134107+AlanMunhoz@users.noreply.github.com>
2018-11-15 17:00:01 -0200

    Random jokes in JavaLibrary.

--- JavaLib/src/main/java/com/devandroid/javalib/RequestJokes.java
+++ JavaLib/src/main/java/com/devandroid/javalib/RequestJokes.java
@@ -1,9 +1,25 @@
 package com.devandroid.javalib;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 public class RequestJokes {
 
+    private static List<String> lstJokes = Arrays.asList(
+            "This is the first funny joke from Java Library!",
+            "This is the second funny joke from Java Library!",
+            "This is the third funny joke from Java Library!",
+            "This is the fourth funny joke from Java Library!",
+            "This is the fifth funny joke from Java Library!",
+            "This is the last funny joke from Java Library!");
+
+
     public String getJoke() {
-        return "This is a funny joke from Java Library!";
+
+        Random random = new Random();
+        return lstJokes.get(random.nextInt(lstJokes.size()));
     }
 
 }

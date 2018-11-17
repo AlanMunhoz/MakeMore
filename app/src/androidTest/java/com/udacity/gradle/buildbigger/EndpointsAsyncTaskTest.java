beff62a91e4c31405061fa253bab2c84fd7d4745
AlanMunhoz <36134107+AlanMunhoz@users.noreply.github.com>
2018-11-17 17:35:12 -0200

    AsyncTask test.

--- app/src/androidTest/java/com/udacity/gradle/buildbigger/EndpointsAsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/buildbigger/EndpointsAsyncTaskTest.java
@@ -1,15 +1,11 @@
 package com.udacity.gradle.buildbigger;
 
 import android.support.test.runner.AndroidJUnit4;
-
 import com.udacity.gradle.builditbigger.EndpointsAsyncTask;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-
 import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.fail;
@@ -18,31 +14,52 @@ import static junit.framework.Assert.fail;
 public class EndpointsAsyncTaskTest implements EndpointsAsyncTask.CallbackAsyncTask {
 
     CountDownLatch countDownLatch;
-    String joke;
+    String strJoke = null;
 
     @Test
     public void returnAsyncTaskEmptyTest() {
 
         try
         {
+            /**
+             * initialize a simple gate
+             */
             countDownLatch = new CountDownLatch(1);
+
+            /**
+             * starts joke request
+             */
             new EndpointsAsyncTask().execute(this);
-            countDownLatch.await(10, TimeUnit.SECONDS);
-            assertNotNull("AsyncTask return is null", joke);
-            assertFalse("AsyncTask return is empty", joke.isEmpty());
+
+            /**
+             * wait until countDownLath reach zero or pass 5 seconds
+             */
+            countDownLatch.await(5, TimeUnit.SECONDS);
+
+            /**
+             * verify if asyncTask returns a valid string
+             */
+            assertNotNull("AsyncTask return is null", strJoke);
+            assertFalse("AsyncTask return is empty", strJoke.isEmpty());
         }
         catch(InterruptedException e)
         {
             fail(e.getMessage());
             e.printStackTrace();
         }
-
     }
 
     @Override
     public void resultOnPostExecute(String strJoke) {
 
-        this.joke = strJoke;
+        /**
+         * Update result to compare
+         */
+        this.strJoke = strJoke;
+
+        /**
+         * Unblock gate to verify asyncTask resut
+         */
         countDownLatch.countDown();
     }
 }
